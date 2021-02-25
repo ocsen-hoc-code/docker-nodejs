3645194111c0af49b06c18f1c040af2161dbd799
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2021-02-25 18:40:10 +0700

    update host

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 // Constants
 const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = process.env.HOST || '0.0.0.0';
 
 // App
 const app = express();
